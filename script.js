df77e4c325f1926c4c607a52bc7c47237ab66349
hennigk <kayla.hennig@gmail.com>
2015-11-12 18:15:57 +0000

    added count variables

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ Promise.promisifyAll(require("request"));
 
 /*this program calculates the total cost to purchase all variants
 of lamps and wallets
-all items listed require tax and for all items shipping is avaiable
-
+all items listed require tax and for all items shipping is available
 */
 
 request.getAsync("http://shopicruit.myshopify.com/products.json")
@@ -14,14 +13,22 @@ request.getAsync("http://shopicruit.myshopify.com/products.json")
     return body.products;
 }).then(function(products){
     var cost = 0;
+    var lampCount = 0;
+    var walletCount = 0;
     for (var i = 0; i < products.length; i++) {
         if (products[i].product_type === 'Lamp' ||  products[i].product_type === 'Wallet'){
+            if (products[i].product_type === 'Lamp'){
+                lampCount += products[i].variants.length;
+            }
+            if (products[i].product_type === 'Wallet'){
+                walletCount += products[i].variants.length;
+            }
             for (var j = 0; j < products[i].variants.length; j++){
                 cost += Number(products[i].variants[j].price); 
             }
         }   
     }
-    return cost;
-}).then(function(totalCost){
-    console.log("The cost to purchase all variants of lamps and wallets from Shopicruit is: $" + totalCost + " + tax & shipping.")
+    return {lampCount: lampCount, walletCount: walletCount, cost: cost};
+}).then(function(totals){
+    console.log("The cost to purchase all " + totals.lampCount + " variants of lamps and all " + totals.walletCount + " variants of wallets from Shopicruit is: $" + totals.cost + " + tax & shipping.");
 })
